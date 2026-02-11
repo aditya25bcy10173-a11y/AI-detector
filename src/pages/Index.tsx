77052fb2e63fe8ea5ed7d530b237c2da5bd783ef
Aditya Kumar <aditya.25bcy10173@vitbhopal.ac.in>
2026-02-11 18:02:02 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import FeatureCards from "@/components/FeatureCards";
 import Footer from "@/components/Footer";
-import ParticlesBackground from "@/components/particlesBackground";
+import ParticlesBackground from "@/components/ParticlesBackground";
 
 const Index = () => (
   <div className="min-h-screen bg-background relative">
